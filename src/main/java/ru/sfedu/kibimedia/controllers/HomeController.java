dcca83d9d7abd9e4ce4cdca5ce0d5ea1ae2581a2
DmitryPosadsky <dmitry.posadsky@gmail.com>
2016-05-18 08:45:01 +0300

    Создание заготовки

---
+++ src/main/java/ru/sfedu/kibimedia/controllers/HomeController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sfedu.kibimedia.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author DPosadsky
+ */
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value="/home", method = RequestMethod.GET)
+    public String viewHome() {
+        return "home";
+    }
+    
+    @RequestMapping(value="/home", method = RequestMethod.GET, params = "newPar")
+    public String viewHomeWithParams() {
+        System.out.println("newPar");
+        return "home";
+    }
+    
+}
