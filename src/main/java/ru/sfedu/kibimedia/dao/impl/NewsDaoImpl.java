8a6fd119c01167d11202d6736b50682f1ef1fd2d
Michael L <dexter88retxed@gmail.com>
2016-05-20 14:43:19 +0300

    Создал News для Hibernate

---
+++ src/main/java/ru/sfedu/kibimedia/dao/impl/NewsDaoImpl.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sfedu.kibimedia.dao.impl;
+
+import ru.sfedu.kibimedia.dao.TaskDao;
+import ru.sfedu.kibimedia.tables.News;
+//import dev.dposadsky.java.swingteacherdesktop.utils.HibernateUtils;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+//import org.hibernate.Session;
+//import org.hibernate.criterion.Order;
+//import org.hibernate.criterion.Restrictions;
+/**
+ *
+ * @author Тамара
+ */
+public class NewsDaoImpl implements TaskDao {
+    
+    @Override
+    public void addNews(News news) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(news);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if ((session != null) && (session.isOpen()))
+                session.close();
+        }
+    }
+
+    @Override
+    public void deleteNews(News news) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(news);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if ((session != null) && (session.isOpen()))
+                session.close();
+        }
+    }
+
+    @Override
+    public void deleteNews(int id) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(getTask(id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if ((session != null) && (session.isOpen()))
+                session.close();
+        }
+    }
+
+    @Override
+    public News getNews(int id) throws SQLException {
+        News news = null;
+        
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            news = (News) session.load(News.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if ((session != null) && (session.isOpen()))
+                session.close();
+        }
+        
+        return news;
+    }
+
+    @Override
+    public ArrayList<News> getAllNews() throws SQLException {
+        ArrayList<News> allNews = null;
+        
+        Session session = null;
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            allNews = (ArrayList<News>) session.createCriteria(News.class)
+                    .addOrder(Order.asc("taskNumber")).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if ((session != null) && (session.isOpen()))
+                session.close();
+        }
+        
+        return allNews;
+    }
+    
+}
---
+++ src/main/java/ru/sfedu/kibimedia/tables/News.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.sfedu.kibimedia.tables;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+/**
+ *
+ * @author Mishas
+ */
+@Entity
+@Table(name = "news")
+public class News implements Serializable{
+    
+    private static final long serialVersionUID = -5527566248002296042L;
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+    
+    @Id
+    
+    @Column(name = "id_news")
+    private Integer idNews;
+    
+    @Column(name = "title")
+    private String title;
+    
+    @Column(name = "main_idea")
+    private String mainIdea;
+
+    @Column(name = "text")
+    private String text;
+    
+    @Column(name = "id_writer")
+    private Integer idWriter;
+    
+    @Column(name = "data")
+    private Date data; 
+
+    public Integer getIdNews() {
+        return idNews;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMainIdea() {
+        return mainIdea;
+    }
+
+    public void setMaimIdea(String mainIdea) {
+        this.mainIdea = mainIdea;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    public Integer idWriter() {
+        return idWriter;
+    }
+
+    public void setIdWriter(Integer idWriter) {
+        this.idWriter = idWriter;
+    }
+    
+    @Override
+    public String toString() {
+        return this.title;
+    }
+
+    public Date getDate() {
+        return data;
+    }
+
+    public void setImports(Date data) {
+        this.data = data;
+    }
+}
+    
